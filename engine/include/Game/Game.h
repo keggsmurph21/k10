5c0be45061686783bcb9dbb4e79463117a71ca5d
Kevin Murphy <keggsmurph21@gmail.com>
2020-02-09 21:07:10 -0500

    Game: Set defaults for some boolean state-tracking stuff

--- engine/include/Game/Game.h
+++ engine/include/Game/Game.h
@@ -69,16 +69,17 @@ private:
     int m_victory_points_goal;
 
     size_t m_deck_index{ 0 };
-    bool m_can_steal;
-    bool m_has_rolled;
-    bool m_is_game_over;
-    bool m_is_trade_accepted;
+    bool m_can_steal{ false };
+    bool m_has_rolled{ false };
+    bool m_is_game_over{ false };
+    bool m_is_trade_accepted{ false };
     Dice m_dice;
     Robber m_robber;
     std::optional<Trade> m_current_trade;
     int m_turn{ 0 };
     std::vector<Player*> m_players;
     Player* m_current_player; // snakes at beginning
+
     Player* m_has_largest_army{ nullptr };
     Player* m_has_longest_road{ nullptr };
 };
