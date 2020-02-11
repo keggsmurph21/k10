f2d17e7ae6bb15d23ea06e9f00c031f3ffe24523
Kevin Murphy <keggsmurph21@gmail.com>
2020-02-11 00:45:40 -0500

    Core: Add AllBuildings and AllDevelopmentCards constants
    This allows us to iterate easily over these enums, which we'll want
    to do in Game methods.

--- engine/include/Core/DevelopmentCard.h
+++ engine/include/Core/DevelopmentCard.h
@@ -12,6 +12,11 @@ enum class DevelopmentCard {
     YearOfPlenty,
 };
 
+static const DevelopmentCard AllDevelopmentCards[] = {
+    DevelopmentCard::Knight,       DevelopmentCard::Monopoly,     DevelopmentCard::RoadBuilding,
+    DevelopmentCard::VictoryPoint, DevelopmentCard::YearOfPlenty,
+};
+
 std::ostream& operator<<(std::ostream&, DevelopmentCard);
 
 } // namespace k10engine
