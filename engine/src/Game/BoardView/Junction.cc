0f40e4af7664b4a92c9f2a8bf7dba0e7f0ca94af
Kevin Murphy <keggsmurph21@gmail.com>
2020-02-12 20:26:36 -0800

    Junction: Improve ostream operator<< output

--- engine/src/Game/BoardView/Junction.cc
+++ engine/src/Game/BoardView/Junction.cc
@@ -7,19 +7,19 @@ std::ostream& operator<<(std::ostream& os, const Junction& junction)
 {
     os << "BoardView::Junction{ " << junction.index() << ", ";
     if (junction.has_settlement()) {
-        os << "(settlement: " << junction.owner()->index() << ") ";
+        os << "(settlement: " << junction.owner()->index() << "), ";
     } else if (junction.has_city()) { // NOLINT(bugprone-branch-clone)
-        os << "(city: " << junction.owner()->index() << ") ";
+        os << "(city: " << junction.owner()->index() << "), ";
     } else if (junction.is_settleable()) {
-        os << "(not yet settled) ";
+        os << "(not yet settled), ";
     } else {
-        os << "(not settleable) ";
+        os << "(not settleable), ";
     }
     if (junction.port_exchange_rate() == 0) {
         os << "(not a port) }";
     } else {
-        os << "(port: " << junction.port_exchange_rate() << " @ " << junction.port_resources()
-           << ") }";
+        os << "Port{ " << junction.port_exchange_rate() << ", " << junction.port_resources()
+           << "} }";
     }
     return os;
 }
