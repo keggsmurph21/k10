78fb76269fac99d7fff0353afb3dffa52633fd90
Kevin Murphy <keggsmurph21@gmail.com>
2020-02-16 16:59:35 -0500

    Test/Game: Stub out testing func

---
+++ engine/src/Game/Test/Game.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+#include "Game/Game.h"
+#include "Test/catch.h"
+
+namespace k10engine::Game {
+
+TEST_CASE()
+{
+    SECTION()
+    {
+        // something
+    }
+}
+
+} // namespace k10engine::Game
