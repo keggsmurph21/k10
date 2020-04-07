fee8dc7476c8287a9c16968174fde248c0c55266
Kevin Murphy <keggsmurph21@gmail.com>
2020-04-06 17:59:03 -0700

    LRUCache: Add a basic templated LRU cache

---
+++ engine/include/Util/LRUCache.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <functional>
+
+#include "Util/DoublyLinkedList.h"
+
+template<typename Key, typename Value>
+class LRUCache {
+private:
+    struct Node {
+        Node(const Key& k, const Value& v)
+            : key(k)
+            , value(v)
+        {
+        }
+        Key key;
+        Value value;
+    };
+
+    static void noop(Key&, Value&) {}
+
+    using Storage = DoublyLinkedList<Node*>;
+
+public:
+    LRUCache(std::function<Value(const Key&)> on_miss, size_t capacity)
+        : m_on_miss(on_miss)
+        , m_on_eviction(noop)
+        , m_capacity(capacity)
+        , m_size(0)
+    {
+        assert(capacity > 0);
+    }
+    LRUCache(std::function<Value(const Key&)> on_miss,
+             std::function<void(Key&, Value&)> on_eviction,
+             size_t capacity)
+        : m_on_miss(on_miss)
+        , m_on_eviction(on_eviction)
+        , m_capacity(capacity)
+        , m_size(0)
+    {
+        assert(capacity > 0);
+    }
+
+    ~LRUCache() { clear(); }
+
+    Value& get(const Key& key)
+    {
+        for (auto it = m_storage.begin(); !it.is_end(); ++it) {
+            auto* node = *it;
+            if (node->key == key) {
+                touch(it);
+                return node->value;
+            }
+        }
+        if (m_size == m_capacity) {
+            auto* evicted_node = m_storage.pop();
+            m_on_eviction(evicted_node->key, evicted_node->value);
+            delete evicted_node;
+            --m_size;
+        }
+        auto value = m_on_miss(key);
+        auto* node = new Node(key, value);
+        m_storage.prepend(node);
+        ++m_size;
+        return node->value;
+    }
+
+    size_t size() const { return m_size; }
+    size_t capacity() const { return m_capacity; }
+
+    void clear()
+    {
+        for (auto* node : m_storage) {
+            delete node;
+        }
+    }
+
+private:
+    Storage m_storage;
+    size_t m_size;
+    size_t m_capacity;
+    std::function<Value(const Key&)> m_on_miss;
+    std::function<void(Key&, Value&)> m_on_eviction;
+
+    void touch(typename Storage::Iterator it)
+    {
+        auto* node = *it;
+        m_storage.remove(it);
+        m_storage.prepend(node);
+    }
+};
---
+++ engine/src/Util/Test/LRUCache.cc
@@ -0,0 +1,97 @@
+#include <cstring>
+
+#include "Test/catch.h"
+#include "Util/LRUCache.h"
+
+static int g_counter = 0;
+
+template<typename T>
+int get(const T& /* unused */)
+{
+    return ++g_counter;
+}
+
+TEST_CASE("LRUCache", "[Util][Util.LRUCache]")
+{
+    SECTION("constructor & destructor")
+    {
+        LRUCache<double, int> int_cache(get<double>, 10);
+        LRUCache<std::string, int> str_cache(get<std::string>, 10);
+        LRUCache<int*, int> int_ptr_cache(get<int*>, 10);
+    }
+
+    SECTION("get()")
+    {
+        LRUCache<int, int> cache(get<int>, 10);
+        REQUIRE(cache.size() == 0);
+        REQUIRE(cache.get(10) == 1);
+        REQUIRE(cache.size() == 1);
+        REQUIRE(cache.get(11) == 2);
+        REQUIRE(cache.size() == 2);
+        REQUIRE(cache.get(12) == 3);
+        REQUIRE(cache.size() == 3);
+        REQUIRE(cache.get(10) == 1);
+        REQUIRE(cache.get(11) == 2);
+        REQUIRE(cache.get(12) == 3);
+        REQUIRE(cache.size() == 3);
+    }
+
+    SECTION("eviction w/ callback")
+    {
+        std::vector<int> evictions;
+
+        LRUCache<int, int> cache(
+            get<int>, [&](int&, int& value) { evictions.push_back(value); }, 3);
+
+        REQUIRE(cache.get(10) == 4);
+        REQUIRE(cache.get(11) == 5);
+        REQUIRE(cache.get(12) == 6);
+        REQUIRE(cache.size() == 3);
+
+        // force a miss & eviction
+        REQUIRE(cache.get(13) == 7);
+        REQUIRE(cache.size() == 3);
+
+        // non-evicted keys shouldn't change
+        REQUIRE(cache.get(11) == 5);
+        REQUIRE(cache.get(12) == 6);
+
+        // evicted key should be refetched (causing another eviction)
+        REQUIRE(cache.get(10) == 8);
+
+        REQUIRE(cache.size() == 3);
+        REQUIRE(evictions == std::vector<int>{ 4, 7 });
+    }
+
+    SECTION("eviction w/o callback")
+    {
+        LRUCache<int, int> cache(get<int>, 3);
+
+        REQUIRE(cache.get(10) == 9);
+        REQUIRE(cache.get(11) == 10);
+        REQUIRE(cache.get(12) == 11);
+        REQUIRE(cache.size() == 3);
+
+        REQUIRE(cache.get(13) == 12);
+        REQUIRE(cache.size() == 3);
+
+        REQUIRE(cache.get(11) == 10);
+        REQUIRE(cache.get(12) == 11);
+
+        REQUIRE(cache.get(10) == 13);
+
+        REQUIRE(cache.size() == 3);
+    }
+
+    SECTION("lettuce example")
+    {
+        LRUCache<std::string, int> counts(get<std::string>, 4);
+
+        counts.get("hello");
+        counts.get("lettuce");
+        counts.get("hey");
+        counts.get("what");
+        counts.get("is");
+        counts.get("beadly");
+    }
+}
