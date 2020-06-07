804db4002c38ad25097779a5ae0e90c6c87b6ba5
Kevin Murphy <keggsmurph21@gmail.com>
2020-06-06 17:36:29 -0700

    Server: Dispatch ::handle() on Request::Type

--- engine/include/Server/Server.h
+++ engine/include/Server/Server.h
@@ -17,7 +17,16 @@ private:
     [[nodiscard]] bool on_connect(int fd, char* client_ip) override;
     [[nodiscard]] bool on_read(int fd, char* buf, int len) override;
 
-    const Response* handle(const Request&);
+    const Response* handle(const Request*);
+    const Response* handle_register_user(const RegisterUserRequest*);
+    const Response* handle_new_game(const NewGameRequest*);
+    const Response* handle_join_game(const JoinGameRequest*);
+    const Response* handle_leave_game(const LeaveGameRequest*);
+    const Response* handle_start_game(const StartGameRequest*);
+    const Response* handle_make_move(const MakeMoveRequest*);
+    const Response* handle_query(const QueryRequest*);
+    const Response* handle_register_listener(const RegisterListenerRequest*);
+    const Response* handle_unregister_listener(const UnregisterListenerRequest*);
 };
 
 } // namespace k10engine::Server
--- engine/src/Server/Server.cc
+++ engine/src/Server/Server.cc
@@ -11,13 +11,6 @@ bool Server::on_connect(int fd, char* client_ip)
     return true;
 }
 
-// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
-const Response* Server::handle(const Request& request)
-{
-    (void)request;
-    assert(false);
-}
-
 bool Server::on_read(int fd, char* buf, int len)
 {
     // FIXME: Clean up the Request* and Response* !
@@ -25,10 +18,103 @@ bool Server::on_read(int fd, char* buf, int len)
     auto* request = Request::parse(buf, len);
     if (request == nullptr)
         assert(false);
-    auto* response = handle(*request);
+    auto* response = handle(request);
+    delete request;
     if (response == nullptr)
         assert(false);
+    delete response;
     return true;
+}
+
+const Response* Server::handle(const Request* request)
+{
+    assert(request != nullptr);
+    switch (request->m_type) {
+    case Request::Type::RegisterUser:
+        return handle_register_user(static_cast<const RegisterUserRequest*>(request));
+    case Request::Type::NewGame:
+        return handle_new_game(static_cast<const NewGameRequest*>(request));
+    case Request::Type::JoinGame:
+        return handle_join_game(static_cast<const JoinGameRequest*>(request));
+    case Request::Type::LeaveGame:
+        return handle_leave_game(static_cast<const LeaveGameRequest*>(request));
+    case Request::Type::StartGame:
+        return handle_start_game(static_cast<const StartGameRequest*>(request));
+    case Request::Type::MakeMove:
+        return handle_make_move(static_cast<const MakeMoveRequest*>(request));
+    case Request::Type::Query:
+        return handle_query(static_cast<const QueryRequest*>(request));
+    case Request::Type::RegisterListener:
+        return handle_register_listener(static_cast<const RegisterListenerRequest*>(request));
+    case Request::Type::UnregisterListener:
+        return handle_unregister_listener(static_cast<const UnregisterListenerRequest*>(request));
+    default:
+        assert(false);
+    }
+    return nullptr;
+}
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+const Response* Server::handle_register_user(const RegisterUserRequest* request)
+{
+    (void)request;
+    assert(false);
+}
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+const Response* Server::handle_new_game(const NewGameRequest* request)
+{
+    (void)request;
+    assert(false);
+}
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+const Response* Server::handle_join_game(const JoinGameRequest* request)
+{
+    (void)request;
+    assert(false);
+}
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+const Response* Server::handle_leave_game(const LeaveGameRequest* request)
+{
+    (void)request;
+    assert(false);
+}
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+const Response* Server::handle_start_game(const StartGameRequest* request)
+{
+    (void)request;
+    assert(false);
+}
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+const Response* Server::handle_make_move(const MakeMoveRequest* request)
+{
+    (void)request;
+    assert(false);
+}
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+const Response* Server::handle_query(const QueryRequest* request)
+{
+    (void)request;
+    assert(false);
+}
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+const Response* Server::handle_register_listener(const RegisterListenerRequest* request)
+{
+    (void)request;
+    assert(false);
+}
+
+// NOLINTNEXTLINE(readability-convert-member-functions-to-static)
+const Response* Server::handle_unregister_listener(const UnregisterListenerRequest* request)
+{
+    (void)request;
+    assert(false);
 }
 
 } // namespace k10engine::Server
