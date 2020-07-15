0b60f623e51c34e38071fa753ae3417517c9ad41
Kevin Murphy <keggsmurph21@gmail.com>
2020-07-15 15:56:33 -0700

    Game::Store: Add tests

--- engine/src/Game/Store_test.cc
+++ engine/src/Game/Store_test.cc
@@ -1,15 +1,96 @@
 #include <cstdlib>
 #include <unistd.h>
 
+#include "Board/Store.h"
 #include "Game/Store.h"
+#include "Scenario/Store.h"
 #include "Test/catch.h"
+
+namespace k10engine {
+
+Game::Game*
+make_game(Board::Name board_name, Scenario::Name scenario_name, size_t n_players, size_t victory_points_goal)
+{
+    auto b = Board::Store::the().by_name(board_name);
+    auto s = Scenario::Store::the().by_name(scenario_name);
+    auto p = Scenario::Parameters{ Scenario::IterationType::Fixed,
+                                   Scenario::IterationType::Fixed,
+                                   Scenario::IterationType::Fixed,
+                                   Scenario::IterationType::Fixed,
+                                   n_players,
+                                   victory_points_goal };
+    return Game::Game::initialize(b, s, p);
+}
 
 TEST_CASE("Store", "[Game][Game.Store]")
 {
-    char game_store_path_template[] = "/tmp/k10-engine-game-store-XXXXXX";
-    const std::string name = mkdtemp(game_store_path_template);
-    auto gs = k10engine::Game::Store(name, 1);
-    gs.get(100);
-    gs.get(0xdeadbeefdeadbeef);
-    unlink(name.c_str());
+    SECTION("Reading games that don't exist")
+    {
+        char game_store_path_template[] = "/tmp/k10-engine-game-store-XXXXXX";
+        const char* path = mkdtemp(game_store_path_template);
+        if (path == nullptr) {
+            perror("mkdtemp");
+            assert(false);
+        }
+
+        auto store = Game::Store(path, 1);
+        REQUIRE(store.get(100) == nullptr);
+        REQUIRE(store.get(0xdeadbeefdeadbeef) == nullptr);
+
+        if (rmdir(path) != 0) {
+            perror("rmdir");
+            assert(false);
+        }
+    }
+
+    SECTION("Storing a game to disk")
+    {
+        char game_store_path_template[] = "/tmp/k10-engine-game-store-XXXXXX";
+        const char* path = mkdtemp(game_store_path_template);
+        if (path == nullptr) {
+            perror("mkdtemp");
+            assert(false);
+        }
+
+        auto store = Game::Store(path, 1);
+
+        // Need to be careful with pointer ownership here, since the Game::Store expects
+        // to be able to "delete" at will.
+        const auto get_game = []() -> Game::Game* {
+            return make_game(Board::Name::Single, Scenario::Name::Single, 1, 3);
+        };
+
+        auto* expected_game_0 = get_game();
+        store.set(0, get_game());
+
+        // force a cache eviction on expected_game_0
+        auto* expected_game_1 = get_game();
+        store.set(1, get_game());
+
+        // check that expected_game_1 is still good to go
+        auto* actual_game_1 = store.get(1);
+        REQUIRE(actual_game_1 != nullptr);
+        REQUIRE(*actual_game_1 == *expected_game_1);
+
+        // load in expected_game_0 from disk ...
+        auto* actual_game_0 = store.get(0);
+        REQUIRE(actual_game_0 != nullptr);
+        REQUIRE(*actual_game_0 == *expected_game_0);
+
+        // check that expected_game_1 is still good to go
+        actual_game_1 = store.get(1);
+        REQUIRE(actual_game_1 != nullptr);
+        REQUIRE(*actual_game_1 == *expected_game_1);
+
+        delete expected_game_0;
+        delete expected_game_1;
+
+        // FIXME: Add an "rm -rf" implementation ...
+        // if (rmdir(path) != 0) {
+        // perror("rmdir");
+        // assert(false);
+        //}
+    }
 }
+
+} // namespace k10engine
